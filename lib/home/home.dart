530895234b389897abfc3c25dee44a61b6581690
Minsuyeon-0329 <msc1767@gmail.com>
2022-05-15 22:39:57 +0900

    test3

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,54 @@
 import 'package:bce_app/alarm/alarm.dart';
 import "package:flutter/material.dart";
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: getDrawer(),
+      drawer: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('assets/profile.jpeg'),
+                ),
+                otherAccountsPictures: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/profilejpeg'),
+                  )
+                ],
+                //accountEmail: Text('@gamil.com'),
+                //accountName: Text('daaam'),
+                onDetailsPressed: () {
+                  print('press details');
+                },
+                decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40),
+                    )), accountName: null, accountEmail: null,
+              ),
+              ListTile(
+                leading: Icon(Icons.home, color: Colors.grey[850]),
+                title: Text('Home'), onTap: () { print('Home is clicked'); },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings, color: Colors.grey[850]),
+                title: Text('Setting'), onTap: () { print('Setting is clicked'); },
+                trailing: Icon(Icons.add),
+              ),
+              ListTile(
+                leading: Icon(Icons.question_answer, color: Colors.grey[850]),
+                title: Text('Q&A'), onTap: () { print('Q&A is clicked'); },
+                trailing: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Center(child: Text('의생명융합공학부')),
         backgroundColor: Color.fromRGBO(10, 101, 83, 1),
@@ -23,59 +65,18 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget getDrawer() {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/profile.jpeg'),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/profilejpeg'),
-                )
-              ],
-              //accountEmail: Text('@gamil.com'),
-              //accountName: Text('daaam'),
-              onDetailsPressed: () {
-                print('press details');
-              },
-              decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
-                  )), accountName: null, accountEmail: null,
-            ),
-            ListTile(
-              leading: Icon(Icons.home, color: Colors.grey[850]),
-              title: Text('Home'), onTap: () { print('Home is clicked'); },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings, color: Colors.grey[850]),
-              title: Text('Setting'), onTap: () { print('Setting is clicked'); },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.question_answer, color: Colors.grey[850]),
-              title: Text('Q&A'), onTap: () { print('Q&A is clicked'); },
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   Widget getBody() {
     return Column(
       children: [
-        SizedBox(
-          height: 100,
-          width: 200,
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+                color: Color.fromRGBO(10, 101, 83, 1)
+              
+            ),
+          ),
         ),
         Expanded(
           child: GridView.count(
@@ -124,7 +125,7 @@ class HomePage extends StatelessWidget {
 
   Widget getFooter() {
     return Padding(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
         child: Container(
           width: double.infinity,
           height: 56,
