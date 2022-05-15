325e4f5d006ff09e30113f723212d68c03ec499d
Minsuyeon-0329 <msc1767@gmail.com>
2022-05-16 00:20:11 +0900

    BCEPage

---
+++ lib/bce/bce.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BCEPage extends StatelessWidget {
+  const BCEPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
