58c0cd64d7f83590a0acfc845fa42c6da7394283
Stella7778 <81565165+Stella7778@users.noreply.github.com>
2022-06-01 23:58:04 +0900

    alarm 수정

--- lib/alarm/alarm.dart
+++ lib/alarm/alarm.dart
@@ -1,30 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlarmPage extends StatelessWidget {
   const AlarmPage({Key? key}) : super(key: key);
-  static const bool alarmExist = true;
+  static const bool alarmExist = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
           centerTitle: true,
-          title: const Text('알람'),
+          title: const Text('알림'),
           backgroundColor: const Color.fromRGBO(10, 101, 83, 1),
         ),
-        body: getBody());
-  }
-
-  Widget getBody() {
-    return Container(
-        child: alarmExist
-            ? ListView(children: [
-                Center(
-                  child: Text('알림이 없습니다.'),
-                )
-              ])
-            : ListView(
-                children: [Card(child: Text('코스모스(칼 세이건)이/가 3일 뒤 반납일입니다.')), Card(child: Text('예약한 총, 균, 쇠(제레..이/가 대출 가능합니다.'))],
-              ));
+        body: Container(
+            child: alarmExist
+                ? const Center(
+                    child: Text(
+                      '알림이 없습니다.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                : ListView(
+                    padding: const EdgeInsets.all(10),
+                    children: const [
+                      Card(
+                        child: ListTile(
+                            leading: Icon(Icons.timer, size: 40),
+                            title: Text(
+                              '반납',
+                            ),
+                            subtitle: Text(
+                              '코스모스(칼 세이건)',
+                            )),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Icon(Icons.book, size: 40),
+                          title: Text('예약'),
+                          subtitle: Text(
+                            '총, 균, 쇠(제레미 다이아몬드)',
+                          ),
+                        ),
+                      ),
+                    ],
+                  )));
   }
 }
